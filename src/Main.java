bac57fe63716aa64c3e583d97b9d58bf9c64c471
mahri.antongiovann23 <razberry779@gmail.com>
2023-01-24 11:25:19 -0600

    add another print statement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 public class Main{
     public static void main (String[] args){
-System.out.println("Hi I'm on replit")
+System.out.println("Hi I'm on replit");
+        System.out.println("I'm on intellij");
         //create a frame
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
